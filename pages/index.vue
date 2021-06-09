742c3b856f2cfd6c3795b0e5a8367373907cef88
AhmedFromThePark <73194048+AhmedFromThePark@users.noreply.github.com>
2021-06-09 21:13:49 +0200

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <b-navbar>
+        <template #brand>
+            <b-navbar-item tag="router-link" :to="{ path: '/' }">
+                <img
+                    src="..\assets\Logo.jpg"
+                    alt="Made by Serhat Sadoglu and Markus Ullrich"
+                >
+            </b-navbar-item>
+        </template>
+        <template #start>
+            <b-navbar-item href="">
+                Home
+            </b-navbar-item>
+            <b-navbar-item href="#">
+                Documentation
+            </b-navbar-item>
+            <b-navbar-dropdown label="Info">
+                <b-navbar-item href="#">
+                    About
+                </b-navbar-item>
+                <b-navbar-item href="#">
+                    Contact
+                </b-navbar-item>
+            </b-navbar-dropdown>
+        </template>
+    </b-navbar>
+</template> 
